484eb1e60baf7823b93d83a2bed7c0597952bc7c
NickAnderson019 <nick@thalana.co.za>
2018-09-01 17:59:38 +0200

    Added interrupt functions

--- prac_4_andnic019_stcdev001.py
+++ prac_4_andnic019_stcdev001.py
@@ -3,29 +3,82 @@
 
 import RPi.GPIO as GPIO
 import time
+import Adafruit_MCP3008
+import os
 GPIO.setwarnings(False)
 
 # init variables & constants
 GPIO.setmode(GPIO.BCM) # use GPIO pin numbering
 delay = 300 # button debounce time
+lightmax = 898 # maximum expected LDR value
 
 # set pin names
 resetpin = 19
 freqpin = 16
 stoppin = 20
 displaypin = 21
+CLK = 11
+MISO = 9
+MOSI = 10
+CS = 8
 
 # setup pin modes
 GPIO.setup(resetpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(freqpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(stoppin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(displaypin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(MOSI, GPIO.OUT)
+GPIO.setup(MISO, GPIO.IN)
+GPIO.setup(CLK, GPIO.OUT)
+GPIO.setup(CS, GPIO.OUT)
+
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, mosi=MOSI, miso=MISO)
+
+# handle reset button presses
+def reset_pushed(channel):
+    print("Reset pushed")
+GPIO.add_event_detect(resetpin, GPIO.RISING, reset_pushed, delay);
+
+# handle freq button presses
+def freq_pushed(channel):
+    print("Frequency pushed")
+GPIO.add_event_detect(freqpin, GPIO.RISING, freq_pushed, delay);
+
+# handle stop button presses
+def stop_pushed(channel):
+    print("Stop pushed")
+GPIO.add_event_detect(stoppin, GPIO.RISING, stop_pushed, delay);
+
+# handle display button presses
+def display_pushed(channel):
+    print("Display pushed")
+GPIO.add_event_detect(displaypin, GPIO.RISING, display_pushed, delay);
 
 #try-finally block to handle GPIO cleanup and robust termination
 try:
+    # print header line
+    print ("Time \t Timer \t Pot \t Temp \t Light")
+    
     #loop for programme execution    
     while True: # make the code run until an exception is thrown
-        foo = 0 #for use with empty programme loop (pure button control)
+        #foo = 0 #for use with empty programme loop (pure button control)
+        
+        # read MCP raw input values
+        tempraw = mcp.read_adc(0)
+        lightraw = mcp.read_adc(1)
+        potraw = mcp.read_adc(2)
+        
+        # convert raw values to meaningful output numbers
+        actualtime = 0
+        timer = 0
+        tempvolt = 3.3*(tempraw/1023)# convert raw value to voltage for use in formula
+        temp = (tempvolt-0.5)/(0.01)
+        light = 100*(lightraw/lightmax)
+        pot = 3.3*(potraw/1023)
+        
+        # Display values
+        print ("%d \t %d \t %.1f V \t %.0f C \t %.0f%%" % (actualtime, timer, pot, temp, light))
+        time.sleep(0.5) # delay for a half second
         
 finally:
     GPIO.cleanup()
