31b4e5f8259e6f9da18a258a77feacc4ae6bd5fe
Devin Stickells <stickells777@gmail.com>
2018-09-03 23:39:37 +0200

    All working; bit of a code cleanup

--- prac_4_andnic019_stcdev001.py
+++ prac_4_andnic019_stcdev001.py
@@ -16,6 +16,8 @@ time_start = time.time() # reference time to calculate timer value
 freq_arr = [0.5,1,2] # array of delay time values in seconds
 time_delay = 0.5 # default delay is 500ms
 started = True
+stoppedcount = 0
+stoppedcache = ""
 
 # set pin names
 resetpin = 19
@@ -45,6 +47,8 @@ def reset_pushed(channel):
     time_start = time.time() # reset time of timer
     print("\033c", end='') #clears the console, but only when running from terminal. No way to clear IDE shell
     print("Reset pushed")
+    # print header line
+    print ('{:10}{:10}{:8}{:8}{:8}'.format("Time", "Timer", "Pot", "Temp", "Light"))
 GPIO.add_event_detect(resetpin, GPIO.RISING, reset_pushed, delay);
 
 # handle freq button presses
@@ -54,21 +58,55 @@ def freq_pushed(channel):
     if (index==2): index=0  
     else: index +=1 
     time_delay = freq_arr[index] # iterate to next delay value
+    
     print("Frequency pushed")
     print("Time delay = " + str(time_delay) + "s")
 GPIO.add_event_detect(freqpin, GPIO.RISING, freq_pushed, delay);
 
 # handle stop button presses
 def stop_pushed(channel):
-    print("Stop/Start pushed")
     global started
     started = not(started)
+    if (started): print("Start pushed")
+    else:  print("Stop pushed")
+    
+    # reset the stopped cache for use by the Display button
+    global stoppedcache
+    stoppedcache = ""
+    global stoppedcount
+    stoppedcount = 0
 GPIO.add_event_detect(stoppin, GPIO.RISING, stop_pushed, delay);
 
 # handle display button presses
 def display_pushed(channel):
+    global stoppedcache
     print("Display pushed")
+    print(stoppedcache)
 GPIO.add_event_detect(displaypin, GPIO.RISING, display_pushed, delay);
+
+def getreadings():
+    # read MCP raw input values
+    tempraw = mcp.read_adc(0)
+    lightraw = mcp.read_adc(1)
+    potraw = mcp.read_adc(2)
+    
+    # convert raw values to meaningful output numbers
+    current_ticks = time.time() # current number of seconds since 1978
+    timer_seconds = round(current_ticks - time_start) # value of timer
+    
+    timer = str(datetime.timedelta(seconds=timer_seconds)) # Convert seconds to correct format
+    actualtime = datetime.datetime.fromtimestamp(current_ticks).strftime('%H:%M:%S') # Get the current device time
+    tempvolt = 3.3*(tempraw/1023) # convert raw value to voltage for use in formula
+    temp = (tempvolt-0.5)/(0.01)
+    light = 100*(lightraw/lightmax)
+    pot = 3.3*(potraw/1023)
+    
+    readings = '{:10}{:6}{:6.1f} V {:4.0f} C {:4.0f}%'.format(actualtime, timer, pot, temp, light)
+    return readings
+
+def wait():
+    time_corrector = (time.time()-time_start)%time_delay # account for function run time.
+    time.sleep(time_delay-time_corrector) # delay for time delay value   
 
 #try-finally block to handle GPIO cleanup and robust termination
 try:
@@ -77,31 +115,19 @@ try:
     
     #loop for programme execution    
     while True: # make the code run until an exception is thrown
+        readings = getreadings();
+        
         if (started):
-            #foo = 0 #for use with empty programme loop (pure button control)
-            
-            # read MCP raw input values
-            tempraw = mcp.read_adc(0)
-            lightraw = mcp.read_adc(1)
-            potraw = mcp.read_adc(2)
-            
-            # convert raw values to meaningful output numbers
-            current_ticks = time.time() # current number of seconds since 1978
-            timer_seconds = round(current_ticks - time_start) # value of timer
-            
-            timer = str(datetime.timedelta(seconds=timer_seconds)) # Convert seconds to correct format
-            actualtime = datetime.datetime.fromtimestamp(current_ticks).strftime('%H:%M:%S') # Get the current device time
-            tempvolt = 3.3*(tempraw/1023) # convert raw value to voltage for use in formula
-            temp = (tempvolt-0.5)/(0.01)
-            light = 100*(lightraw/lightmax)
-            pot = 3.3*(potraw/1023) 
-            
             # Display values
-            print ('{:10}{:6}{:6.1f} V {:4.0f} C {:4.0f}%'.format(actualtime, timer, pot, temp, light))
-            time_corrector = (time.time()-time_start)%time_delay # account for function run time.
-            time.sleep(time_delay-time_corrector) # delay for time delay value
+            print (readings)
         else:
-            foo = 0
+            if (stoppedcount < 5) :
+                # add current set of readings to cache
+                stoppedcache = stoppedcache + readings
+                if (stoppedcount < 4): stoppedcache += "\n"
+                stoppedcount += 1
+                
+        wait()     
 
 finally:
     GPIO.cleanup()
